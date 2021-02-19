13eea8216d02ee45ba51cb694b3f54f2f7815e5c
mo_n <mo_n.tt@outlook.com>
2021-02-19 17:15:22 +0800

    add link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,15 +7,15 @@ function Header() {
   return (
     <header className="sticky shadow-lg top-0 z-40 w-full mx-auto bg-blue-500 flex-none flex items-center py-2 px-6">
       <Link to="/" className="inline-flex h-6 cursor-pointer text-white">
-        <span className="text-lg"><HomeIcon className="relative inline-block w-6 box-content mr-2 -top-0.5" />Chocobo Colors</span>
+        <span className="text-lg"><HomeIcon className="relative inline-block w-7 box-content mr-2 -top-0.5"/>Chocobo Colors</span>
       </Link>
       <div className="flex-auto"></div>
-      <Link
-        to="/"
+      <a
+        href="https://github.com/mo-n/ffxivchocobo"
         className="inline-flex w-6 h-6 p-2 box-content cursor-pointer text-white"
       >
         <GithubIcon />
-      </Link>
+      </a>
     </header>
   );
 }
