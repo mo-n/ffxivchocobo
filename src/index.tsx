8f400c04815fea75a03ec956a6ffae6a34050bdf
mo_n <mo_n.tt@outlook.com>
2021-02-22 18:19:04 +0800

    fix  basename

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
